241871bf239532d68218cbf5e25c82a779dfc1c9
Ram Rachure <46816331+ramkrishna2k3@users.noreply.github.com>
2023-04-03 17:29:09 +0530

    updated
    update example

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -30,6 +30,10 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
+    
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thank you & Happy Learning";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
